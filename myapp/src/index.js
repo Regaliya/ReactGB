8f8f7989d844226c99f01e879bc4383eecb59f9c
Елена <83121369+Regaliya@users.noreply.github.com>
2021-10-12 15:59:21 +0300

    homework1

---
+++ myapp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const myName = 'Helen';
+
+ReactDOM.render(
+
+    <App name={myName}/>,
+
+  document.getElementById('root')
+);
+
